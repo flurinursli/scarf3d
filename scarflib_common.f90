ff6e4ab75728df4452e30cf5bc92474ef1a87dfe
Walter@Chaputschin <walter.imperatori@sed.ethz.ch>
2020-02-04 19:05:24 +0100

    still exploring best IO

--- scarflib_common.f90
+++ scarflib_common.f90
@@ -136,20 +136,26 @@ MODULE SCARFLIB_COMMON
       REAL(FPP),                    DIMENSION(:,:,:),           INTENT(IN) :: V                                  !< RANDOM FIELD
       CHARACTER(LEN=*),                                         INTENT(IN) :: FILENAME                           !< NAME OF OUTPUT FILE
       INTEGER(IPP),                                   OPTIONAL, INTENT(IN) :: NWRITERS
-      INTEGER(IPP)                                                         :: I                                  !< COUNTER
-      INTEGER(IPP)                                                         :: COLOR, NEWCOMM, RANK, NTASKS
+      INTEGER(IPP)                                                         :: I, J, N, NMAX                               !< COUNTER
+      INTEGER(IPP)                                                         :: COLOR, NEWCOMM, WRTCOMM
+      INTEGER(IPP)                                                         :: RANK, RCVRANK, REQUEST
+      INTEGER(IPP)                                                         :: NTASKS, MAXTASKS
       INTEGER(IPP)                                                         :: NEWTYPE, IERR, FH                  !< MPI STUFF
       INTEGER(KIND=MPI_OFFSET_KIND)                                        :: FILESIZE, OFFSET                   !< MPI STUFF
+      INTEGER(IPP),                 DIMENSION(MPI_STATUS_SIZE)             :: STATUS
       INTEGER(IPP),                 DIMENSION(3)                           :: SUBSIZES, STARTS                   !< ARRAYS FOR DERIVED DATATYPE
       INTEGER(IPP),                 ALLOCATABLE, DIMENSION(:)              :: RANK0, RANK1
+      INTEGER(IPP),                 ALLOCATABLE, DIMENSION(:)              :: NEW2WORLD
+      REAL(FPP),                    ALLOCATABLE, DIMENSION(:)              :: BUFFER
+
+      REAL(REAL64)                                                         :: TICTOC
 
       !-----------------------------------------------------------------------------------------------------------------------------
 
       ALLOCATE(RANK0(NWRITERS), RANK1(NWRITERS))
 
-
       ! ORGANIZE PROCESSES INTO "NWRITERS" COMMUNICATORS
-      CALL MPI_SPLIT_TASK(WORLD_SIZE, NWRITERS, RANK0, RANK1)
+      CALL SPLIT_TASK(WORLD_SIZE, NWRITERS, RANK0, RANK1)
 
       ! LOWEST RANK MUST BE ZERO
       RANK0 = RANK0 - 1
@@ -166,6 +172,12 @@ MODULE SCARFLIB_COMMON
       CALL MPI_COMM_RANK(NEWCOMM, RANK, IERR)
       CALL MPI_COMM_SIZE(NEWCOMM, NTASKS, IERR)
 
+      ALLOCATE(NEW2WORLD(0:NTASKS - 1))
+
+      CALL MAP_RANKS(NEWCOMM, MPI_COMM_WORLD, NEW2WORLD)
+
+      ! IF (RANK == 0) PRINT*, 'RANKMAP ', WORLD_RANK, NTASKS, ' -- ', NEW2WORLD
+
       COLOR = 0
 
       ! NOW CREATE A NEW COMMUNICATOR MADE ONLY BY THOSE PROCESSES HAVING RANK=0 WITHIN EACH "NEWCOMM"
@@ -175,18 +187,300 @@ MODULE SCARFLIB_COMMON
       CALL MPI_COMM_SPLIT(MPI_COMM_WORLD, COLOR, WORLD_RANK, WRTCOMM, IERR)
 
       !<<<< TEMPORARY CALL
-      CALL MPI_COMM_SIZE(WRTCOMM, I, IERR); PRINT*, 'WRITERS ', I
+      ! CALL MPI_COMM_SIZE(WRTCOMM, I, IERR); PRINT*, 'WRITERS ', I
 
-      DO J = 0, NTASKS - 1
+      MAXTASKS = NTASKS
 
+      CALL MPI_ALLREDUCE(MPI_IN_PLACE, MAXTASKS, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, IERR)
 
+      ! IF (RANK == 0) PRINT*, 'TASKS ', WORLD_RANK, MAXTASKS, NTASKS
 
+      N = SIZE(V)
 
+      NMAX = N
 
+      CALL MPI_ALLREDUCE(MPI_IN_PLACE, NMAX, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, IERR)
 
-      ENDDO
+      ALLOCATE(BUFFER(NMAX))
+
+      ! EACH PROCESS SENDS ITS DATA TO WRITER INSIDE COMMUNICATOR "NEWCOMM"
+      CALL MPI_ISEND(V, N, REAL_TYPE, 0, 1, NEWCOMM, REQUEST, IERR)
+
+      ! ONLY WRITERS CAN WORK ON FILES
+      IF (RANK .EQ. 0) THEN
+
+        CALL MPI_FILE_OPEN(WRTCOMM, FILENAME, MPI_MODE_CREATE + MPI_MODE_WRONLY, MPI_INFO_NULL, FH, IERR)
+
+        FILESIZE = INT(NPTS(1), MPI_OFFSET_KIND) * INT(NPTS(2), MPI_OFFSET_KIND) * INT(NPTS(3), MPI_OFFSET_KIND)
+
+        CALL MPI_FILE_SET_SIZE(FH, FILESIZE, IERR)
+
+        OFFSET = 0_MPI_OFFSET_KIND
+
+        DO J = 0, MAXTASKS - 1
+
+          CALL WATCH_START(TICTOC, WRTCOMM)
+
+          IF (J .LE. NTASKS - 1) THEN
+
+            CALL MPI_RECV(BUFFER, SIZE(BUFFER), REAL_TYPE, MPI_ANY_SOURCE, MPI_ANY_TAG, NEWCOMM, STATUS, IERR)
+
+            ! RANK THAT SENT DATA
+            RCVRANK = STATUS(MPI_SOURCE)
+
+            ! MAP LOCAL INTO GLOBAL RANK
+            RCVRANK = NEW2WORLD(RCVRANK)
+
+          ENDIF
+
+          CALL WATCH_STOP(TICTOC, WRTCOMM); PRINT*, 'RECV ', WORLD_RANK, REAL(TICTOC, FPP)
+
+          ! TRANSFORM TO GLOBAL RANK
+          SUBSIZES(:) = GE(:, RCVRANK) - GS(:, RCVRANK) + 1
+          STARTS(:)   = GS(:, RCVRANK) - 1
+
+          CALL WATCH_START(TICTOC, WRTCOMM)
+
+          CALL MPI_TYPE_CREATE_SUBARRAY(3, NPTS, SUBSIZES, STARTS, MPI_ORDER_FORTRAN, REAL_TYPE, NEWTYPE, IERR)
+
+          CALL MPI_TYPE_COMMIT(NEWTYPE, IERR)
+
+          CALL WATCH_STOP(TICTOC, WRTCOMM); PRINT*, 'DERIVED ', WORLD_RANK, REAL(TICTOC, FPP)
+
+          !CALL MPI_FILE_SET_SIZE(FH, FILESIZE, IERR)
+
+          CALL MPI_FILE_SET_VIEW(FH, OFFSET, REAL_TYPE, NEWTYPE, 'NATIVE', MPI_INFO_NULL, IERR)
+
+          CALL WATCH_START(TICTOC, WRTCOMM)
+          CALL MPI_FILE_WRITE_ALL(FH, BUFFER, PRODUCT(SUBSIZES), REAL_TYPE, MPI_STATUS_IGNORE, IERR)
+          CALL WATCH_STOP(TICTOC, WRTCOMM); PRINT*, 'WRITE ', WORLD_RANK, REAL(TICTOC, FPP)
+
+          CALL MPI_TYPE_FREE(NEWTYPE, IERR)
+
+        ENDDO
+
+        CALL MPI_FILE_CLOSE(FH, IERR)
+
+      ENDIF
+
+      DEALLOCATE(RANK0, RANK1, NEW2WORLD, BUFFER)
+
+      CALL MPI_BARRIER(MPI_COMM_WORLD, IERR)
+
+      CALL MPI_COMM_FREE(NEWCOMM, IERR)
+      CALL MPI_COMM_FREE(WRTCOMM, IERR)
 
     END SUBROUTINE SCARF3D_WRITE_ONE
+
+    ! SUBROUTINE SCARF3D_WRITE_ONE(V, FILENAME, NWRITERS)
+    !
+    !   REAL(FPP),                    DIMENSION(:,:,:),           INTENT(IN) :: V                                  !< RANDOM FIELD
+    !   CHARACTER(LEN=*),                                         INTENT(IN) :: FILENAME                           !< NAME OF OUTPUT FILE
+    !   INTEGER(IPP),                                   OPTIONAL, INTENT(IN) :: NWRITERS
+    !   INTEGER(IPP)                                                         :: I, J, N, NMAX                               !< COUNTER
+    !   INTEGER(IPP)                                                         :: COLOR, NEWCOMM, WRTCOMM
+    !   INTEGER(IPP)                                                         :: RANK, RCVRANK, REQUEST
+    !   INTEGER(IPP)                                                         :: NTASKS, MAXTASKS
+    !   INTEGER(IPP)                                                         :: NEWTYPE, IERR, FH                  !< MPI STUFF
+    !   INTEGER(KIND=MPI_OFFSET_KIND)                                        :: FILESIZE, OFFSET                   !< MPI STUFF
+    !   INTEGER(IPP),                 DIMENSION(MPI_STATUS_SIZE)             :: STATUS
+    !   INTEGER(IPP),                 DIMENSION(3)                           :: SUBSIZES, STARTS                   !< ARRAYS FOR DERIVED DATATYPE
+    !   INTEGER(IPP),                 ALLOCATABLE, DIMENSION(:)              :: RANK0, RANK1
+    !   INTEGER(IPP),                 ALLOCATABLE, DIMENSION(:)              :: NEW2WORLD
+    !   REAL(FPP),                    ALLOCATABLE, DIMENSION(:)              :: BUFFER
+    !
+    !   !-----------------------------------------------------------------------------------------------------------------------------
+    !
+    !   ALLOCATE(RANK0(NWRITERS), RANK1(NWRITERS))
+    !
+    !   ! ORGANIZE PROCESSES INTO "NWRITERS" COMMUNICATORS
+    !   CALL SPLIT_TASK(WORLD_SIZE, NWRITERS, RANK0, RANK1)
+    !
+    !   ! LOWEST RANK MUST BE ZERO
+    !   RANK0 = RANK0 - 1
+    !   RANK1 = RANK1 - 1
+    !
+    !   DO I = 1, NWRITERS
+    !     IF ( (WORLD_RANK .GE. RANK0(I)) .AND. (WORLD_RANK .LE. RANK1(I)) ) COLOR = I
+    !   ENDDO
+    !
+    !   ! CREATE NEW COMMUNICATOR
+    !   CALL MPI_COMM_SPLIT(MPI_COMM_WORLD, COLOR, WORLD_RANK, NEWCOMM, IERR)
+    !
+    !   ! PROCESS ID AND COMMUNICATOR SIZE
+    !   CALL MPI_COMM_RANK(NEWCOMM, RANK, IERR)
+    !   CALL MPI_COMM_SIZE(NEWCOMM, NTASKS, IERR)
+    !
+    !   ALLOCATE(NEW2WORLD(0:NTASKS - 1))
+    !
+    !   CALL MAP_RANKS(NEWCOMM, MPI_COMM_WORLD, NEW2WORLD)
+    !
+    !   COLOR = 0
+    !
+    !   ! NOW CREATE A NEW COMMUNICATOR MADE ONLY BY THOSE PROCESSES HAVING RANK=0 WITHIN EACH "NEWCOMM"
+    !   IF (RANK .EQ. 0) COLOR = 1
+    !
+    !   ! CREATE NEW COMMUNICATOR
+    !   CALL MPI_COMM_SPLIT(MPI_COMM_WORLD, COLOR, WORLD_RANK, WRTCOMM, IERR)
+    !
+    !   MAXTASKS = NTASKS + 1
+    !
+    !   CALL MPI_ALLREDUCE(MPI_IN_PLACE, MAXTASKS, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, IERR)
+    !
+    !   N = SIZE(V)
+    !
+    !   NMAX = N
+    !
+    !   CALL MPI_ALLREDUCE(MPI_IN_PLACE, NMAX, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, IERR)
+    !
+    !   ALLOCATE(BUFFER(NMAX))
+    !
+    !   IF (RANK .EQ. 0) THEN
+    !
+    !     CALL MPI_FILE_OPEN(WRTCOMM, FILENAME, MPI_MODE_CREATE + MPI_MODE_WRONLY, MPI_INFO_NULL, FH, IERR)
+    !
+    !     FILESIZE = INT(NPTS(1), MPI_OFFSET_KIND) * INT(NPTS(2), MPI_OFFSET_KIND) * INT(NPTS(3), MPI_OFFSET_KIND)
+    !
+    !     CALL MPI_FILE_SET_SIZE(FH, FILESIZE, IERR)
+    !
+    !     OFFSET = 0_MPI_OFFSET_KIND
+    !
+    !     CALL MPI_ISEND(V, N, REAL_TYPE, 0, 0, NEWCOMM, REQUEST, IERR)
+    !
+    !   ENDIF
+    !
+    !   DO J = 1, MAXTASKS - 1
+    !
+    !     IF (RANK .EQ. J) THEN
+    !       CALL MPI_ISEND(V, N, REAL_TYPE, 0, 0, NEWCOMM, REQUEST, IERR)
+    !       ! print*, 'rank ', WORLD_RANK, ' sending to rank 0', j
+    !     endif
+    !
+    !
+    !     IF (RANK .EQ. 0) THEN
+    !
+    !       IF (J .LE. NTASKS) THEN
+    !
+    !         CALL MPI_IRECV(BUFFER, SIZE(BUFFER), REAL_TYPE, J - 1, 0, NEWCOMM, REQUEST, IERR)
+    !
+    !         ! MAP LOCAL INTO GLOBAL RANK
+    !         RCVRANK = NEW2WORLD(J - 1)
+    !
+    !       ENDIF
+    !
+    !       ! TRANSFORM TO GLOBAL RANK
+    !       SUBSIZES(:) = GE(:, RCVRANK) - GS(:, RCVRANK) + 1
+    !       STARTS(:)   = GS(:, RCVRANK) - 1
+    !
+    !       CALL MPI_TYPE_CREATE_SUBARRAY(3, NPTS, SUBSIZES, STARTS, MPI_ORDER_FORTRAN, REAL_TYPE, NEWTYPE, IERR)
+    !
+    !       CALL MPI_TYPE_COMMIT(NEWTYPE, IERR)
+    !
+    !       CALL MPI_FILE_SET_VIEW(FH, OFFSET, REAL_TYPE, NEWTYPE, 'NATIVE', MPI_INFO_NULL, IERR)
+    !
+    !       ! WAIT FOR RECEIVING FROM (J-1)-TH PROCESS
+    !       IF (J .LE. NTASKS) CALL MPI_WAIT(REQUEST, MPI_STATUS_IGNORE, IERR)
+    !
+    !       CALL MPI_FILE_WRITE_ALL(FH, BUFFER, PRODUCT(SUBSIZES), REAL_TYPE, MPI_STATUS_IGNORE, IERR)
+    !
+    !       CALL MPI_TYPE_FREE(NEWTYPE, IERR)
+    !
+    !     ENDIF
+    !
+    !     CALL MPI_BARRIER(NEWCOMM, IERR)
+    !
+    !   ENDDO
+    !
+    !   IF (RANK .EQ. 0) CALL MPI_FILE_CLOSE(FH, IERR)
+    !
+    !   DEALLOCATE(RANK0, RANK1, NEW2WORLD, BUFFER)
+    !
+    !   CALL MPI_BARRIER(MPI_COMM_WORLD, IERR)
+    !
+    !   CALL MPI_COMM_FREE(NEWCOMM, IERR)
+    !   CALL MPI_COMM_FREE(WRTCOMM, IERR)
+    !
+    ! END SUBROUTINE SCARF3D_WRITE_ONE
+
+    ! --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- *
+    !===============================================================================================================================
+    ! --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- *
+
+    SUBROUTINE SCARF3D_WRITE_ONE2(V, FILENAME, NWRITERS)
+
+      REAL(FPP),                    DIMENSION(:,:,:),           INTENT(IN) :: V                                  !< RANDOM FIELD
+      CHARACTER(LEN=*),                                         INTENT(IN) :: FILENAME                           !< NAME OF OUTPUT FILE
+      INTEGER(IPP),                                   OPTIONAL, INTENT(IN) :: NWRITERS
+
+      !-----------------------------------------------------------------------------------------------------------------------------
+
+
+      
+
+
+    END SUBROUTINE SCARF3D_WRITE_ONE2
+
+    ! --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- *
+    !===============================================================================================================================
+    ! --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- *
+
+    SUBROUTINE SPLIT_TASK(NPTS, NTASKS, I0, I1)
+
+      ! DISTRIBUTE "NPTS" POINTS AMONGST "NTASKS" PROCESSES AND, FOR EACH PROCESS, RETURN THE LOWEST AND HIGHEST INDEX.
+
+      INTEGER(IPP),                        INTENT(IN)  :: NPTS                          !< NUMBER OF POINTS TO BE SPLIT
+      INTEGER(IPP),                        INTENT(IN)  :: NTASKS                     !< NUMBER OF MPI PROCESSES
+      INTEGER(IPP), DIMENSION(0:NTASKS-1), INTENT(OUT) :: I0, I1                     !< 1ST/LAST INDEX
+      INTEGER(IPP)                                     :: P                          !< COUNTER
+
+      !------------------------------------------------------------------------------------------------------------------------------
+
+      DO P = 0, NTASKS - 1
+        I0(P) = 1 + INT( REAL(NPTS, FPP) / REAL(NTASKS, FPP) * REAL(P, FPP) )
+        I1(P) = INT( REAL(NPTS, FPP) / REAL(NTASKS, FPP) * REAL(P + 1, FPP) )
+      ENDDO
+
+    END SUBROUTINE SPLIT_TASK
+
+    ! --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- *
+    !===============================================================================================================================
+    ! --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- *
+
+    SUBROUTINE MAP_RANKS(SRC_COMM, DEST_COMM, DEST_RANKS)
+
+      ! MAP RANKS FROM LOCAL TO GLOBAL COMMUNICATOR
+
+      INTEGER(IPP),                         INTENT(IN)    :: SRC_COMM                     !< COMMUNICATOR
+      INTEGER(IPP),                         INTENT(IN)    :: DEST_COMM
+      INTEGER(IPP), DIMENSION(:),           INTENT(INOUT) :: DEST_RANKS                   !< RANKS AFTER BEING MAPPED TO GLOBAL GROUP
+      INTEGER(IPP)                                        :: I                        !< COUNTER
+      INTEGER(IPP)                                        :: IERR, SRC_GROUP              !< MPI STUFF
+      INTEGER(IPP)                                        :: DEST_GROUP
+      INTEGER(IPP)                                        :: NTASKS
+      INTEGER(IPP), DIMENSION(SIZE(DEST_RANKS))           :: SRC_RANKS
+
+      !-----------------------------------------------------------------------------------------------------------------------------
+
+      NTASKS = SIZE(SRC_RANKS)
+
+      ! RANKS IN SOURCE COMMUNICATOR
+      DO I = 1, NTASKS
+        SRC_RANKS(I) = I - 1
+      ENDDO
+
+      ! GROUP ASSOCIATED TO SOURCE COMMUNICATOR
+      CALL MPI_COMM_GROUP(SRC_COMM, SRC_GROUP, IERR)
+
+      ! GROUP ASSOCIATED TO DESTINATION COMMUNICATOR
+      CALL MPI_COMM_GROUP(DEST_COMM, DEST_GROUP, IERR)
+
+      ! MAP RANKS IN "GROUP" INTO RANKS IN "MPI_GROUP_WORLD"
+      CALL MPI_GROUP_TRANSLATE_RANKS(SRC_GROUP, NTASKS, SRC_RANKS, DEST_GROUP, DEST_RANKS, IERR)
+
+      CALL MPI_GROUP_FREE(SRC_GROUP, IERR)
+      CALL MPI_GROUP_FREE(DEST_GROUP, IERR)
+
+    END SUBROUTINE MAP_RANKS
 
     ! --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- * --- *
     !===============================================================================================================================
