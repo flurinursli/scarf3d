641a106db52477315a8c7e19ede7c7e9a0a51576
Walter@Chaputschin <walter.imperatori@sed.ethz.ch>
2020-03-16 18:39:56 +0100

    developing interface for c++

--- src/scarf3d.h
+++ src/scarf3d.h
@@ -10,70 +10,59 @@ extern "C"{
 
   enum algorithm {fft, spec};
 
-  typedef struct{
-    const int acf, rescale, pad;
-    const int[3] fs, fe;
-    const fpp ds, dh;
-    const fpp sigma, hurst;
-    const fpp mute, taper;
-    const fpp[3] cl;
-    fpp* poi;
-    fpp* stats;
-    const fpp* x;
-    const fpp* y;
-    const fpp* z;
-  } scarf_data;
+  void scarf_struct_initialize(const int fs[3], const int fe[3], const fpp ds, const int acf, const fpp cl[3], const fpp sigma, const int,   &
+                               const fpp hurst, const fpp dh, const fpp* poi, const fpp mute, const fpp taper, const int rescale, const int pad);
 
+  void scarf_unstruct_initialize(const fpp* x, const fpp* y, const fpp* z, const fpp dh, const fpp cl[3], const fpp sigma, const int,  &
+                                 const fpp hurst, const fpp* poi, const fpp mute, const fpp taper, const int rescale, const int pad);
 
+  void scarf_execute(const int seed, fpp* field, fpp stats[8]);
 
-
-  scarf_struct scarf_struct_initialize(const int[3] fs, const int[3] fe, const fpp ds, const fpp[3] cl, const fpp sigma,
-                                       const fpp hurst, const fpp dh, const fpp* poi, const fpp mute, const fpp taper,
-                                       const int rescale, const int pad);
-
-  void scarf_struct_execute(scarf_struct S, const int seed, fpp* field);
-
-  scarf_unstruct scarf_unstruct_initialize(const fpp* x, const fpp* y, const fpp* z, const fpp dh, const fpp[3] cl, const fpp sigma,
-                                           const fpp hurst, const fpp dh, const fpp* poi, const fpp mute, const fpp taper,
-                                           const int rescale, const int pad);
-
-  void scarf_unstruct_execute(scarf_unstruct S, const int seed, fpp* field);
+  void finalize();
 
 }
+
 
 namespace Scarf3D{
 
   template <algorithm method> class Initialize{
     private:
-      scarf_data scarf_obj;
     public:
       // structured mesh
-      Initialize(const int[3] fs, const int[3] fe, const fpp ds, const fpp cl[3], const fpp sigma,  &
+      Initialize(const int[3] fs, const int[3] fe, const fpp ds, const int acf, const fpp cl[3], const fpp sigma,  &
                  const fpp hurst = 0, const fpp dh = ds, const fpp* poi, const fpp mute = -1, const fpp taper = -1, const int rescale = 0, const int pad = 0){
 
-                   scarf_obj = c_scarf3d_struct_initialize(fs, fe, ds, cl, sigma, hurst, dh, poi, mute, taper, rescale, pad);
+         if method == fft{
+            void scarf_struct_initialize(fs, fe, ds, acf, cl, sigma, 0, hurst, dh, poi, mute, taper, rescale, pad);
+         }
+         else{
+            void scarf_struct_initialize(fs, fe, ds, acf, cl, sigma, 1, hurst, dh, poi, mute, taper, rescale, pad);
+         }
+
+      };
+
+      // unstructured mesh
+      Initialize(const fpp* x, const fpp* y, const fpp* z, const fpp dh, const fpp cl[3], const fpp sigma,  &
+                 const fpp hurst = 0, const fpp* poi, const fpp mute = -1, const fpp taper = -1, const int rescale = 0, const int pad = 0){
+
+                   if method == fft{
+                     void scarf_unstruct_initialize(x, y, z, dh, acf, cl, sigma, 0, hurst, poi, mute, taper, rescale, pad);
+                   }
+                   else{
+                     void scarf_unstruct_initialize(x, y, z, dh, acf, cl, sigma, 1, hurst, poi, mute, taper, rescale, pad);
+                   }
 
                  };
 
-      // unstructured mesh
-      Initialize(const fpp* x, const fpp* y, const fpp* z, const fpp ds, const fpp cl[3], const fpp sigma,  &
-                 const fpp hurst = 0, const fpp dh = ds, const fpp* poi, const fpp mute = -1, const fpp taper = -1, const int rescale = 0, const int pad = 0){};
-
-      ~Initialize();
-
-      void execute(scarf_data, const int seed, fpp* field);
-   };
-
-
-
-
-
-
-      {
-        c_obj = scarf_struct_initialize(fs, fe, ds, cl, sigma, hurst, dh, poi, mute, taper, rescale, pad);
+      // destructor
+      ~Initialize(){
+        void scarf_finalize();
       };
 
-  };
+      void execute(const int seed, fpp* field, fpp stats[8]){
+        void scarf_execute(seed, field, stats);
+      };
 
+   };
 
 }
